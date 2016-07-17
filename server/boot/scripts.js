c6b37d55d9998d92a82d3589276a143ea3529a71
root <root@ip-172-31-23-87.ap-northeast-1.compute.internal>
2016-07-17 17:59:16 +0800

    modify

--- server/boot/scripts.js
+++ server/boot/scripts.js
@@ -5,17 +5,17 @@
  * @version $Id$
  */
 module.exports=function(app){
-	// var mysqlDs=app.dataSources.mysqlDs;
-	// var user=app.models.yonghu;
+	var mysqlDs=app.dataSources.mysqlDs;
+	var user=app.models.yonghu;
 
 	// // console.log(mysqlDs);
 
-	// mysqlDs.automigrate('yonghu',function(err){
-		// user.create({
-		// 	email:"123456@qq.com",
-		// 	password:"123456"
-		// },function(err,results){
-		// 	console.log(results);
-		// })
-	// })
+	 mysqlDs.automigrate('yonghu',function(err){
+		user.create({
+		 	email:"123456@qq.com",
+		 	password:"123456"
+		},function(err,results){
+			console.log(results);
+		 })
+	 })
 }
