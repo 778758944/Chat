dc0493d57efc54d0ca48a3042db5e422fc54726b
Xiaobo <778758944@qq.com>
2016-08-14 15:11:42 +0800

    modify

--- server/boot/scripts.js
+++ server/boot/scripts.js
@@ -9,6 +9,26 @@ module.exports=function(app){
 	var user=app.models.yonghu;
 	var accessToken=app.models.AccessToken;
 
+	var data=[
+		{
+			email:'778758944@qq.com',
+			password:"123456"
+		},
+		{
+			emial:"123456@qq.com",
+			password:'123456'
+		}
+	]
+
+	user.create(data,function(err,data){
+		if(err){
+			console.log('err',err);
+		}
+		else{
+			console.log('success',data);
+		}
+	})
+
 	// // console.log(mysqlDs);
 
 	 // mysqlDs.automigrate('yonghu',function(err){
