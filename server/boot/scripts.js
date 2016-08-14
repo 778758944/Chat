8188ecd2f0afd2de3aa74722e7750501a3ac9a82
Xiaobo <778758944@qq.com>
2016-08-14 15:09:20 +0800

    modify

---
+++ server/boot/scripts.js
@@ -0,0 +1,34 @@
+/**
+ * 
+ * @authors Your Name (you@example.org)
+ * @date    2016-05-02 21:20:37
+ * @version $Id$
+ */
+module.exports=function(app){
+	var mysqlDs=app.dataSources.mysqlDs;
+	var user=app.models.yonghu;
+	var accessToken=app.models.AccessToken;
+
+	// // console.log(mysqlDs);
+
+	 // mysqlDs.automigrate('yonghu',function(err){
+		// user.create({
+		//  	email:"123456@qq.com",
+		//  	password:"123456"
+		// },function(err,results){
+		// 	console.log(results);
+		//  })
+	 // })
+
+	 // console.log('boot script');
+
+	 // mysqlDs.automigrate('AccessToken',function(err){
+	 // 	if(err){
+	 // 		console.log(err);
+	 // 	}
+	 // 	else{
+	 // 		console.log('accessbuilt');
+	 // 	}
+	 // })
+
+}
