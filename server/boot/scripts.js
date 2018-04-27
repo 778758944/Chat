3fa37914bcdd514c2a48955070e445324b838217
xiaobo <xiaobo.xwt@alibaba-inc.com>
2018-04-27 17:23:13 +0800

    modify

--- server/boot/scripts.js
+++ server/boot/scripts.js
@@ -1,5 +1,5 @@
 /**
- * 
+ *
  * @authors Your Name (you@example.org)
  * @date    2016-05-02 21:20:37
  * @version $Id$
@@ -18,65 +18,65 @@ module.exports=function(app){
 	];
 
 
-	// mysqlDs.automigrate('unread',function(err){
-	// 	if(err){
-	// 		console.log('err');
-	// 	}
-	// 	else{
-	// 		console.log('success');
-	// 	}
-	// })
-
-	
-
-	// mysqlDs.automigrate('pushkey',function(err){
-	// 	if(err){
-	// 		console.log('err');
-	// 	}
-	// 	else{
-	// 		console.log('success');
-	// 	}
-	// })
+	mysqlDs.automigrate('unread',function(err){
+		if(err){
+			console.log('err');
+		}
+		else{
+			console.log('success');
+		}
+	})
 
 
-	// mysqlDs.automigrate('points',function(err){
-	// 	if(err){
-	// 		console.log('err');
-	// 	}
-	// 	else{
-	// 		console.log('success');
-	// 	}
-	// })
 
-	// user.create(data,function(err,data){
-	// 	if(err){
-	// 		console.log('err',err);
-	// 	}
-	// 	else{
-	// 		console.log('success',data);
-	// 	}
-	// })
+	mysqlDs.automigrate('pushkey',function(err){
+		if(err){
+			console.log('err');
+		}
+		else{
+			console.log('success');
+		}
+	})
+
+
+	mysqlDs.automigrate('points',function(err){
+		if(err){
+			console.log('err');
+		}
+		else{
+			console.log('success');
+		}
+	})
+
+	user.create(data,function(err,data){
+		if(err){
+			console.log('err',err);
+		}
+		else{
+			console.log('success',data);
+		}
+	})
 
 	// // console.log(mysqlDs);
 
-	 // mysqlDs.automigrate('yonghu',function(err){
+	 mysqlDs.automigrate('yonghu',function(err){
 		// user.create({
 		//  	email:"123456@qq.com",
 		//  	password:"123456"
 		// },function(err,results){
 		// 	console.log(results);
 		//  })
-	 // })
+	 })
 
 	 // console.log('boot script');
 
-	 // mysqlDs.automigrate('AccessToken',function(err){
-	 // 	if(err){
-	 // 		console.log(err);
-	 // 	}
-	 // 	else{
-	 // 		console.log('accessbuilt');
-	 // 	}
-	 // })
+	 mysqlDs.automigrate('AccessToken',function(err){
+	 	if(err){
+	 		console.log(err);
+	 	}
+	 	else{
+	 		console.log('accessbuilt');
+	 	}
+	 })
 
 }
