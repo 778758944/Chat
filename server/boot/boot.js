2f15b94393a6c1770c3215d8f4807eb305274a47
xiaobo <xiaobo.xwt@alibaba-inc.com>
2018-04-27 15:37:52 +0800

    modify

--- server/boot/boot.js
+++ server/boot/boot.js
@@ -1,5 +1,5 @@
 /**
- * 
+ *
  * @authors Your Name (you@example.org)
  * @date    2016-03-20 16:53:41
  * @version $Id$
@@ -7,6 +7,14 @@
  module.exports=function(app){
  	console.log(app.get("env"));
  	console.log(process.env.NODE_ENV);
+  var User = app.models.Yonghu;
+  Yonghu.create({email: "foo@bar.com", password: "123321q"}, function(err, ins) {
+    if (err) {
+      console.log("error to add user");
+    } else {
+      console.log("success", ins);
+    }
+  })
 
  	var router=app.loopback.Router();
  	router.get('/api/test',function(req,res){
